3252e62d40e40a5ec5fe61a3326cd383a2896bea
WangYixuan12 <wyx990608@sjtu.edu.cn>
2020-08-19 00:43:10 -0400

    more reasonable result

--- scripts/main.py
+++ scripts/main.py
@@ -16,6 +16,7 @@ from shape_completion_training.model.model_runner import ModelRunner
 from rviz_voxelgrid_visuals import conversions
 from rviz_voxelgrid_visuals_msgs.msg import VoxelgridStamped
 from skimage import measure
+import tensorflow as tf
 
 rgb_list = []
 depth_list = []
@@ -28,7 +29,132 @@ unit_scaling = 0.001
 
 is_online = False
 
-trial_path = "/home/deformtrack/catkin_ws/src/probabilistic_shape_completion/shape_completion_training/trials/Flow/August_09_14-36-09_1486a44186"
+    # group_defaults = {
+    #     "NormalizingAE": # NormalizingAE/August_11_02-26-33_c94a291391
+    #         {
+    #             'num_latent_layers': 200,
+    #             'flow': 'Flow/August_09_14-36-09_1486a44186',
+    #             'network': 'NormalizingAE',
+    #             'use_flow_during_inference': False
+    #         },
+    #     "VAE": # VAE/August_11_22-48-49_c94a291391
+    #         {
+    #             'num_latent_layers': 200,
+    #             'network': 'VAE'
+    #         },
+    #     "VAE_GAN": # VAE_GAN/August_12_20-32-01_c94a291391/
+    #         {
+    #             'num_latent_layers': 200,
+    #             'network': 'VAE_GAN',
+    #             'learning_rate': 0.0001,
+    #             'discriminator_learning_rate': 0.00005,
+    #         },
+    #     "Flow": # Flow/August_09_14-36-09_1486a44186
+    #         {
+    #             'batch_size': 1500,
+    #             'network': 'RealNVP',
+    #             'dim': 24,
+    #             'num_masked': 12,
+    #             'learning_rate': 1e-5,
+    #             'translation_pixel_range_x': 10,
+    #             'translation_pixel_range_y': 10,
+    #             'translation_pixel_range_z': 10,
+    #         },3 
+    #     "FlowYCB": # FlowYCB/August_09_14-38-16_1486a44186
+    #         {
+    #             'batch_size': 1500,
+    #             'network': 'RealNVP',
+    #             'dim': 24,
+    #             'num_masked': 12,
+    #             'learning_rate': 1e-5,
+    #             'translation_pixel_range_x': 20,
+    #             'translation_pixel_range_y': 10,
+    #             'translation_pixel_range_z': 10,
+    #             'dataset': 'ycb',
+    #         },
+    #     "3D_rec_gan": # 3D_rec_gan/August_13_20-31-41_c94a291391
+    #         {
+    #             'batch_size': 4,
+    #             'dataset': 'shapenet',
+    #             'network': '3D_rec_gan',
+    #             "learning_rate": 0.0001,
+    #             "gan_learning_rate": 0.00005,
+    #             "num_latent_layers": 2000,
+    #             "is_u_connected": True,
+    #         },
+    #     "NormalizingAE_YCB": # NormalizingAE_YCB/August_15_12-49-28_c94a291391
+    #         {
+    #             'num_latent_layers': 200,
+    #             'flow': 'FlowYCB/August_09_14-38-16_1486a44186',
+    #             'network': 'NormalizingAE',
+    #             'use_flow_during_inference': False,
+    #             'dataset': 'ycb',
+    #             'translation_pixel_range_x': 15,
+    #             'translation_pixel_range_y': 10,
+    #             'translation_pixel_range_z': 10,
+    #             'apply_slit_occlusion': True,
+    #         },
+    #     "VAE_YCB": # VAE_YCB/August_15_20-22-32_c94a291391
+    #         {
+    #             'num_latent_layers': 200,
+    #             'network': 'VAE',
+    #             'dataset': 'ycb',
+    #             'apply_slit_occlusion': True,
+    #             'translation_pixel_range_x': 15,
+    #             'translation_pixel_range_y': 10,
+    #             'translation_pixel_range_z': 10,
+    #         },
+    #     "VAE_GAN_YCB": # VAE_GAN_YCB/August_16_04-27-12_c94a291391
+    #         {
+    #             'num_latent_layers': 200,
+    #             'network': 'VAE_GAN',
+    #             'learning_rate': 0.0001,
+    #             'discriminator_learning_rate': 0.00005,
+    #             'dataset': 'ycb',
+    #             'apply_slit_occlusion': True,
+    #             'translation_pixel_range_x': 15,
+    #             'translation_pixel_range_y': 10,
+    #             'translation_pixel_range_z': 10,
+    #         },
+    #     "3D_rec_gan_YCB": # 3D_rec_gan_YCB/August_16_13-15-52_c94a291391
+    #         {
+    #             'batch_size': 4,
+    #             'dataset': 'shapenet',
+    #             'network': '3D_rec_gan',
+    #             "learning_rate": 0.0001,
+    #             "gan_learning_rate": 0.00005,
+    #             "num_latent_layers": 2000,
+    #             "is_u_connected": True,
+    #             'dataset': 'ycb',
+    #             'apply_slit_occlusion': True,
+    #             'translation_pixel_range_x': 15,
+    #             'translation_pixel_range_y': 10,
+    #             'translation_pixel_range_z': 10,
+    #         },
+    #     "NormalizingAE_YCB_noise": # NormalizingAE_YCB_noise/August_17_04-17-26_c94a291391
+    #         {
+    #             'num_latent_layers': 200,
+    #             'flow': 'FlowYCB/August_09_14-38-16_1486a44186',
+    #             'network': 'NormalizingAE',
+    #             'use_flow_during_inference': False,
+    #             'dataset': 'ycb',
+    #             'translation_pixel_range_x': 15,
+    #             'translation_pixel_range_y': 10,
+    #             'translation_pixel_range_z': 10,
+    #             'apply_slit_occlusion': True,
+    #             'apply_depth_sensor_noise': True,
+    #         },
+    #     "NormalizingAE_noise": # NormalizingAE_noise/August_17_14-29-51_c94a291391
+    #         {
+    #             'num_latent_layers': 200,
+    #             'flow': 'Flow/August_09_14-36-09_1486a44186',
+    #             'network': 'NormalizingAE',
+    #             'use_flow_during_inference': False,
+    #             'apply_depth_sensor_noise': True,
+    #         },
+    # }
+
+trial_path = "/home/deformtrack/catkin_ws/src/probabilistic_shape_completion/shape_completion_training/trials/3D_rec_gan/August_13_20-31-41_c94a291391"
 
 params = {
     'num_latent_layers': 200,
@@ -38,7 +164,7 @@ params = {
     # 'use_final_unet_layer': False,
     # 'simulate_partial_completion': False,
     # 'simulate_random_partial_completion': False,
-    'network': 'VAE',
+    'network': '3D_rec_gan',
     # 'network': 'VAE_GAN',
     # 'network': 'Augmented_VAE',
     # 'network': 'Conditional_VCNN',
@@ -48,7 +174,7 @@ params = {
     # 'flow': 'Flow/August_09_14-36-09_1486a44186'
 }
 
-model_runner = ModelRunner(training=True, trial_path=trial_path, group_name = 'Flow', params = params)
+model_runner = ModelRunner(training=True, trial_path=trial_path, params=params)
 
 def CameraInfo2np(info):
     K = np.zeros((3, 3))
@@ -93,7 +219,7 @@ def read_bagfile():
     cam_topic = "camera_info"
     rgb_topic = "image_color_rect"
     depth_topic = "image_depth_rect"
-    rosbag_name = "/home/deformtrack/catkin_ws/src/cdcpd_test_blender/dataset/shape_comp_1.bag"
+    rosbag_name = "/home/deformtrack/catkin_ws/src/cdcpd_test_blender/dataset/shape_comp_2.bag"
 
     rgb_sub = message_filters.Subscriber(rgb_topic, Image)
     depth_sub = message_filters.Subscriber(depth_topic, Image)
@@ -118,34 +244,44 @@ def read_live():
     print("WARNING")
 
 def main():
-    origin = (-1.2, -1.2, 0.0) # "left most" point
-    shape = (24, 24, 24) # how many grids there
-    scale = 0.1 # how large the grid is
+    origin = (-0.2, -0.2, 1.4) # "left most" point
+    shape = (64, 64, 64) # how many grids there
+    shape_inp = (1, 64, 64, 64, 1)
+    scale = 0.01 # how large the grid is
     rospy.init_node("cdcpd_shape_completion")
-    pub = rospy.Publisher('grid', VoxelgridStamped, queue_size=1)
+    pub_incomp = rospy.Publisher('incomp', VoxelgridStamped, queue_size=1)
+    pub_comp = rospy.Publisher('comp', VoxelgridStamped, queue_size=1)
     if not is_online:
         read_bagfile()
         color_segmentation()
-        for i in range(1):
+        for i in range(5):
             pc = imgs2pc(rgb_list[i], depth_list[i], camera_info_list[i], mask_list[i])
             voxelgrid = pointcloud_to_voxelgrid(pc, scale=scale, origin=origin, shape=shape)
-            print("voxel grid sum")
-            print(voxelgrid.sum())
-            completion = model_runner.model(voxelgrid)
+            voxel_inp = np.zeros(shape_inp)
+            voxel_inp[0, :, :, :, 0] = voxelgrid
+            freesapce = np.zeros(shape_inp)
+            inp = {
+                'known_occ': voxel_inp,
+                'known_free': freesapce,
+            }
+            completion = model_runner.model(inp)
             # print("completion vg:")
-            comp_np = completion.numpy()
-
-            comp_np[comp_np < 0.4] = 0
-            comp_np[comp_np >= 0.4] = 1
+            comp_np = (completion['predicted_occ'].numpy())[0, :, :, :, 0]
+            comp_np[comp_np < 0.0001] = 0
+            comp_np[comp_np >= 0.0001] = 1
 
             # print(comp_np.shape)
             # print(comp_np.sum())
             # print(comp_np)
 
-            # pub.publish(conversions.vox_to_voxelgrid_stamped(comp_np, # Numpy or Tensorflow
-            #                                          scale=scale, # Each voxel is a 1cm cube
-            #                                          frame_id='world', # In frame "world", same as rviz fixed frame
-            #                                          origin=origin)) # Bottom left corner
+            pub_incomp.publish(conversions.vox_to_voxelgrid_stamped(voxelgrid, # Numpy or Tensorflow
+                                                     scale=scale, # Each voxel is a 1cm cube
+                                                     frame_id='world', # In frame "world", same as rviz fixed frame
+                                                     origin=origin)) # Bottom left corner
+            pub_comp.publish(conversions.vox_to_voxelgrid_stamped(comp_np, # Numpy or Tensorflow
+                                                     scale=scale, # Each voxel is a 1cm cube
+                                                     frame_id='world', # In frame "world", same as rviz fixed frame
+                                                     origin=origin)) # Bottom left corner
 
             verts, faces, normals, values = measure.marching_cubes_lewiner(comp_np, 0)
             # Display resulting triangular mesh using Matplotlib. This can also be done
